65ee201549abc969af9439b4a9e8528a8d257853
GenHero <51133244+GenHero@users.noreply.github.com>
2021-02-26 21:03:08 -0500

    got seachbar and post to render

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -10,6 +10,10 @@ const Posts = (props) => {
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map((p)=> {
+        console.log(posts)
+        return <Post key = {p.id} post ={posts} likePost ={likePost} />
+      })}
     </div>
   );
 };
